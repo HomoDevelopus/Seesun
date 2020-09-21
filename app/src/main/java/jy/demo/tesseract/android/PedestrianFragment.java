68d03c41454138ae9bda6a50e43791f299b1ef4a
anyeji <anyeji1220@gmail.com>
2020-09-21 19:47:05 +0900

    update camera function

---
+++ app/src/main/java/jy/demo/tesseract/android/PedestrianFragment.java
@@ -0,0 +1,22 @@
+package jy.demo.tesseract.android;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class PedestrianFragment extends Fragment {
+
+    public PedestrianFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_pedestrian, container, false);
+    }
+}
