5e78ddfdfe680964fa8bd90bc5a9f78b7afcfa6e
anyeji <anyeji1220@gmail.com>
2020-09-22 20:14:58 +0900

    data parsing complete

--- app/src/main/java/jy/demo/tesseract/android/FileUploadUtils.java
+++ app/src/main/java/jy/demo/tesseract/android/FileUploadUtils.java
@@ -2,11 +2,13 @@ package jy.demo.tesseract.android;
 
 import android.util.Log;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 
 import okhttp3.Call;
 import okhttp3.Callback;
@@ -18,13 +20,15 @@ import okhttp3.Response;
 
 
 public class FileUploadUtils {
-    public static void send2Server(File file){
+    private ArrayList<JSONObject> detectedObjs;
+
+    public ArrayList<JSONObject> send2Server(File file){
         RequestBody requestBody = new MultipartBody.Builder()
                 .setType(MultipartBody.FORM)
                 .addFormDataPart("file",file.getName(),RequestBody.create(MultipartBody.FORM,file))
                 .build();
         Request request = new Request.Builder()
-        .url("http://fa5d348f7e0f.ngrok.io/file_upload")
+        .url("http://109efd0415b4.ngrok.io/file_upload")
         .post(requestBody).build();
 
         OkHttpClient client = new OkHttpClient();
@@ -36,14 +40,22 @@ public class FileUploadUtils {
 
             @Override
             public void onResponse(Call call, Response response) throws IOException {
-                Log.d("TEST :: ", response.body().toString());
-//                try {
-//                    JSONObject jsonObject = new JSONObject(response.body().toString());
-//                    Log.d("obj0: ",jsonObject.getString("obj0"));
-//                } catch (JSONException e) {
-//                    e.printStackTrace();
-//                }
+                detectedObjs = new ArrayList<>();
+//                Log.d("GET :: ", response.body().string());
+                try {
+                    JSONObject jsonObject = new JSONObject(response.body().string());
+                    JSONArray jsonArray = jsonObject.getJSONArray("obj");
+//                    JSONArray jsonArray = new JSONArray(response.body().string());
+                    for (int i=0;i<jsonArray.length();i++){
+                        JSONObject jsonObject2 = jsonArray.getJSONObject(i);
+                        Log.d("jsonObject2 :: ", String.valueOf(jsonObject2));
+                        detectedObjs.add(jsonObject2);
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
             }
         });
+        return detectedObjs;
     }
 }
--- app/src/main/java/jy/demo/tesseract/android/MainActivity.java
+++ app/src/main/java/jy/demo/tesseract/android/MainActivity.java
@@ -11,15 +11,30 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.ImageView;
 
 import com.googlecode.tesseract.android.TessBaseAPI;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.android.Utils;
+import org.opencv.core.Mat;
+import org.opencv.core.Point;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
 
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+
 
 public class MainActivity extends AppCompatActivity {
     //카메라
@@ -34,11 +49,14 @@ public class MainActivity extends AppCompatActivity {
     private Camera.ShutterCallback myShutterCallback;
 
     //테서랙트
-    TessBaseAPI tessBaseAPI;//tesseract 관련 클래스 객체
+    private TessBaseAPI tessBaseAPI;//tesseract 관련 클래스 객체
+    private FileUploadUtils fu;
+    private ArrayList<JSONObject> detectedObjs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        fu = new FileUploadUtils();
 
         // 카메라 프리뷰를  전체화면으로 보여주기 위해 셋팅한다.
         supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
@@ -95,18 +113,50 @@ public class MainActivity extends AppCompatActivity {
             public void run() {
                 while(true) {
                     try {
-                        Thread.sleep(1000);
+                        Thread.sleep(1000);//1초 간격으로 사진 촬영
                         mCamera.takePicture(myShutterCallback,
                                 myPictureCallback_RAW, myPictureCallback_JPG);
                         Thread.sleep(500);
-                        FileUploadUtils.send2Server(file);
-
-                    } catch (InterruptedException e) {
+                        dataProcessing(fu.send2Server(file));//response 호출될 때만
+                    } catch (InterruptedException | JSONException e) {
                         e.printStackTrace();
                     }
                 }
             }
         }).start();
+    }
+
+    public void dataProcessing(ArrayList<JSONObject> detectedObjs) throws JSONException {
+        Log.d("<dataprocessing 함수 호출>", "json 데이터 처리");
+        if(detectedObjs == null){
+            return;
+        }
+        for(int i=0;i<detectedObjs.size();i++){
+            String object = detectedObjs.get(i).get("object").toString();
+            float accuracy = Float.parseFloat(detectedObjs.get(i).get("accuracy").toString());
+            float[] location = new float[4];
+            String tmp = detectedObjs.get(i).get("location").toString();
+            tmp = tmp.substring(1,tmp.length()-1);
+//            System.out.println("tmp:"+tmp);
+            for(int j = 0; j < 4 ; j++){
+                location[j] = Float.parseFloat(tmp.substring(1,tmp.length()-1).split(",")[j]);
+            }
+
+            Bitmap myBitmap = BitmapFactory.decodeFile(file.getAbsolutePath()); // 비트맵 생성
+            OpenCVLoader.initDebug(); // 이 코드를 선언해주지않으면 컴파일 에러 발생
+            Mat img =new Mat();
+            Utils.bitmapToMat(myBitmap ,img); // 비트맵을 Mat으로 변환
+            //Draw rectangle
+            //Parameters: mat object for drawing, point coordinates (x1,y1,x2,y2) and color BGR
+            Imgproc.rectangle(img, new Point(location[0],location[1]), new Point(location[3],location[4]), new Scalar(255, 0, 0, 255), 3);
+
+            Bitmap resultBitmap = Bitmap.createBitmap(img.cols(),  img.rows(),Bitmap.Config.ARGB_8888);;
+            Utils.matToBitmap(img, resultBitmap); // Mat을 비트맵으로 변환
+
+            ImageView imageView = (ImageView)findViewById(R.id.image_result);
+            imageView.setImageBitmap(resultBitmap); // 이미지 뷰에 비트맵 출력
+        }
+
     }
 
     public static Camera getCamera(){
@@ -144,7 +194,6 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
-
     @Override
     public void onRequestPermissionsResult(int requestCode,
                                            String permissions[], int[] grantResults) {
@@ -160,6 +209,8 @@ public class MainActivity extends AppCompatActivity {
             }
             return;
         }
-
+    }
+    static {
+        System.loadLibrary("opencv_java4");
     }
 }
